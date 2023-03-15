0ce67941d419537811abf5778e7a1a84fb101b1d
imfariz <farizramadhan07@gmail.com>
2023-03-16 03:57:21 +0800

    Submission Dicoding Basic Web

---
+++ script.js
@@ -0,0 +1,186 @@
+//Import data artikel
+// import { artikel } from "./data.js";
+
+//Membaca perubahan URL
+// window.onpopstate = function() {
+//     //Mendapatkan alamat URL Sekarang
+//     let url = document.location.href;
+
+//     //Memperoleh index terakhir string yang memiliki karakter '#'
+//     let hashIndex = url.lastIndexOf("#/");
+
+//     //Melakukan slicing dari index karakter "#" terakhir, ditambah 1 untuk meninggalkan nilai string "#"
+//     let statusURL = url.slice(hashIndex + 1);
+
+//     //Percabangan untuk melakukan rendering konten artikel sesuai dengan nilai URL
+//     switch (statusURL) {
+//         case "/":
+//             generateDOM(0);
+//             break;
+//         case "/yoga":
+//             generateDOM(1);
+//             break;
+//         case "/workout":
+//             generateDOM(2);
+//             break;
+//         case "/food":
+//             generateDOM(3);
+//             break;
+//         default:
+//             generateDOM(0);
+//     }
+// }
+
+// Data JSON artikel apabila halaman dijalankan tanpa http atau tidak support ES6
+let artikel = [
+    {
+        judul: 'The most important part of fitness goal',
+        paragraf: `<p>
+            The most important part of a fitness goal is setting a clear and achievable objective. Your goal should be specific, measurable, attainable, relevant, and time-bound. This is often referred to as the "SMART" goal-setting framework.
+            <br>
+            Having a clear goal provides direction and focus, helps you stay motivated, and allows you to track your progress over time. It also helps you to plan out the steps necessary to achieve your goal, such as developing a workout routine, making changes to your diet, and establishing healthy habits.
+            <br>
+            However, it's important to remember that achieving your fitness goal requires consistency and commitment. It's not enough to simply set a goal; you must also be willing to put in the effort and make changes to your lifestyle in order to reach your desired outcome. This can involve seeking guidance from a personal trainer or nutritionist, finding a workout buddy for accountability, and celebrating small victories along the way to stay motivated.
+            <br>
+        </p>
+        <figure>
+            <img src="./assets/fitness.jpg" alt="Fitness Image">
+            <figcaption>Photo by <a href="https://unsplash.com/photos/Lx_GDv7VA9M">Sven Mieke (Unsplash)</a></figcaption>
+        </figure>
+        <p>
+            Here's an example of a SMART fitness goal: "I want to lose 10 pounds of body fat in the next 3 months by doing strength training exercises and following a healthy diet plan."
+            <br>
+            In addition to setting a SMART goal, consistency and commitment are also crucial for achieving your fitness goal. It's important to have a workout plan and stick to it, as well as make sustainable changes to your diet and lifestyle. Celebrating small wins along the way and having a support system can also help keep you on track and motivated.
+        </p>`,
+        pathGambar: "./assets/fitness.jpg",
+    },
+    {
+        judul: 'How Yoga works for you',
+        paragraf: `<p>Yoga is an ancient practice that originated in India over 5,000 years ago. It is a holistic system of physical and mental practices designed to promote health and well-being. The word "yoga" comes from the Sanskrit word "yuj," which means to unite or to join, symbolizing the connection between the body, mind, and spirit.</p>
+
+        <p>There are many different styles of yoga, each with its own unique approach to postures, breathwork, and meditation. Some of the most popular styles of yoga include Hatha yoga, Vinyasa yoga, Ashtanga yoga, and Kundalini yoga.</p>
+        <figure>
+            <img src="./assets/yoga.jpg" alt="Yoga Image">
+            <figcaption>Photo by <a href="https://unsplash.com/photos/r6Oz7d4wl3o">Conscious Design (Unsplash)</a></figcaption>
+        </figure>
+        <p>Yoga works by integrating physical postures, breath control, and meditation to promote health and well-being. Here's how each of these components of yoga works:</p>
+        <p>
+            <ol>
+                <li>Physical Postures: Yoga postures, or asanas, help to improve flexibility, strength, balance, and coordination. They also help to increase circulation, reduce inflammation, and improve the functioning of the respiratory, digestive, and immune systems.</li>
+                <li>Breath Control: Pranayama, or breath control, is an important part of yoga practice. Deep breathing exercises help to calm the nervous system, reduce stress, and improve lung function.</li>
+                <li>Meditation: Meditation is an integral part of yoga practice that helps to calm the mind, promote relaxation, and cultivate self-awareness. By focusing on the breath and body sensations, yoga practitioners learn to be more present in the moment and develop a greater awareness of their thoughts, feelings, and physical sensations.</li>
+            </ol>
+        </p>
+        <p>
+        Yoga is a powerful tool for promoting health and well-being, with benefits that extend beyond physical fitness to mental and emotional well-being as well. By integrating physical postures, breath control, and meditation, yoga helps to create a sense of balance and harmony in the body, mind, and spirit. With regular practice, yoga can help to reduce stress, improve physical fitness, enhance mental clarity, and promote spiritual growth.
+        </p>`,
+        pathGambar: "./assets/yoga.jpg",
+    },
+    {
+        judul: 'workout inside',
+        paragraf: `<p>
+        With busy schedules and limited access to gyms, working out at home has become a popular option for many people. Whether you’re looking to lose weight, build muscle, or simply improve your overall fitness, there are plenty of effective workouts that you can do from the comfort of your own home. In this article, we’ll explore some of the best home workouts that you can try today.
+        </p>
+        <p>
+        <ol>
+                <li>Bodyweight exercises are a great way to build strength and improve your overall fitness without any equipment. Some popular bodyweight exercises include push-ups, squats, lunges, and planks. These exercises can be modified to make them easier or more challenging depending on your fitness level.</li>
+                <li>Yoga is a great way to improve flexibility, balance, and strength. There are many online resources available for practicing yoga at home, such as YouTube videos or virtual classes. Whether you’re a beginner or an experienced yogi, there’s a yoga routine that’s right for you.</li>
+                <li>Cardiovascular exercises such as jumping jacks, high knees, and burpees can help you burn calories and improve your cardiovascular health. You don’t need any equipment to perform these exercises, and you can do them in a small space such as your living room.</li>
+                <li>Resistance bands are a great way to add resistance to your workouts without the need for heavy weights. They come in different levels of resistance, making them suitable for beginners and advanced exercisers. You can use resistance bands for a variety of exercises such as bicep curls, tricep extensions, and leg presses.</li>
+                <li>Jumping rope is a fun and effective way to improve your cardiovascular health and burn calories. You only need a small space to jump rope, making it a great option for working out at home. Jumping rope can also help improve your coordination and balance.</li>
+                <li>Many fitness studios and gyms now offer virtual fitness classes that you can join from the comfort of your own home. These classes can range from high-intensity interval training (HIIT) to yoga to dance workouts. Virtual fitness classes are a great way to stay motivated and accountable while working out at home.</li>
+            </ol>
+        </p>
+        <figure>
+            <img src="./assets/workout.jpg" alt="Workout Image">
+            <figcaption>Photo by <a href="https://unsplash.com/photos/P4AIjQw1BuY">Boxed Water (Unsplash)</a></figcaption>
+        </figure>
+        <p>
+        In conclusion, there are many effective workouts that you can do from the comfort of your own home. Whether you prefer bodyweight exercises, yoga, cardiovascular exercises, resistance bands, jump rope, or virtual fitness classes, there’s a workout that’s right for you. By incorporating these workouts into your daily routine, you can improve your overall fitness and lead a healthier lifestyle.
+        </p>`,
+        pathGambar: "./assets/workout.jpg",
+    },
+    {
+        judul: 'Whats good for your belly',
+        paragraf: `<p>
+        Taking care of your digestive system is essential for maintaining overall health and wellness. The foods you eat can have a significant impact on your digestive system, with some foods being better than others at promoting digestive health. In this article, we will discuss some of the best foods that are good for your belly.
+        </p>
+        <p>
+        <ol>
+                <li> Yogurt contains probiotics, which are live bacteria that can help improve the balance of good bacteria in your gut. These good bacteria can aid in digestion and boost your immune system. Choose plain yogurt with live and active cultures for maximum benefit.</li>
+                <li>Ginger is a natural anti-inflammatory and can help alleviate stomach discomfort and nausea. You can add fresh ginger to your tea or smoothies, or incorporate it into your meals for an extra flavor boost.</li>
+                <li> Oatmeal is a great source of soluble fiber, which can help regulate bowel movements and prevent constipation. It is also a low-glycemic food, which means it can help regulate blood sugar levels.</li>
+                <li>Fermented foods like sauerkraut, kimchi, and kefir contain probiotics and can help improve gut health. They also contain enzymes that can aid in digestion and absorption of nutrients.</li>
+                <li>Bananas are a good source of prebiotics, which are non-digestible fibers that feed the good bacteria in your gut. They also contain potassium, which can help regulate blood pressure and promote heart health.</li>
+                <li>Whole grains like brown rice, quinoa, and barley are rich in fiber and can help regulate bowel movements. They also contain B vitamins, which are essential for a healthy digestive system.</li>
+                <li>Leafy greens like kale, spinach, and collard greens are rich in fiber, vitamins, and minerals. They can also help promote the growth of good bacteria in your gut.</li>
+                <li>Nuts and seeds like almonds, walnuts, chia seeds, and flaxseeds are rich in fiber and healthy fats, which can help promote digestive health. They are also a good source of antioxidants, which can help protect against inflammation and disease.</li>
+                <li>Fruits like apples, pears, and berries are high in fiber and can help regulate bowel movements. They also contain antioxidants and other nutrients that can promote overall health.</li>
+                <li>Lean proteins like chicken, fish, and tofu are easy to digest and can provide essential amino acids that are necessary for building and repairing tissues in the body.</li>
+            </ol>
+        </p>
+        <figure>
+            <img src="./assets/food.jpg" alt="Food Image">
+            <figcaption>Photo by <a href="https://unsplash.com/photos/uQs1802D0CQ">Katie Smith (Unsplash)</a></figcaption>
+        </figure>
+        <p>
+        In conclusion, incorporating these foods into your diet can help improve your digestive health and promote overall wellness. By consuming a variety of whole, nutritious foods, you can help support your gut microbiome and reduce the risk of digestive disorders.
+        </p>`,
+        pathGambar: "./assets/food.jpg",
+    }
+]
+
+// JS Document Object Model untuk tombol navigasi
+const navigation = document.getElementsByTagName("a");
+
+//Function JS Document Object Model untuk merubah konten artikel
+let generateDOM = function(index) {
+    document.getElementById("judulArtikel").innerHTML = artikel[index].judul;
+    document.getElementById("artikel").innerHTML = artikel[index].paragraf;
+}
+
+// Memilih artikel apabila halaman dijalankan tanpa http (localhost live server)
+for (let indeks = 0; indeks < navigation.length-1; indeks++) {
+    navigation[indeks].addEventListener("click", function() {
+        let currentStatus = document.getElementsByClassName("active");
+
+        if(currentStatus.length>0) {
+            currentStatus[0].className = currentStatus[0].className.replace("active", "");
+        }
+
+        this.className += "active";
+        generateDOM(indeks);
+    })
+};
+
+//JS Document Object Model untuk memperoleh elemen button
+const button = document.getElementsByTagName('button')[0];
+
+//Membaca perubahan scroll
+let scrolling = false;
+window.onscroll = () => {
+    scrolling = true;
+    // Apabila user melakukan scrolling, maka tombol bantuan "Back To Top" akan menghilang
+    button.style.opacity = "0";
+    button.style.transition = "opacity 0.1s linear";
+}
+
+// Mencari posisi article didalam halaman
+const articleOffset = document.getElementById('artikel').offsetTop;
+
+//Menampilkan tombol bantuan "Back To Top" apabila user tidak melakukan scrolling selama 1,5 detik
+setInterval(() => {
+    // Mendapatkan posisi scroll saat ini
+    let scrollPosition = document.documentElement.scrollTop;
+
+    // Pengecekan apakah scroll sudah berhenti dan memasuki halaman artikel untuk menampilkan tombol bantuan
+    if (scrolling && scrollPosition > articleOffset) {
+        scrolling = false;
+        button.style.opacity = "1";
+        button.style.transition = "opacity 0.5s linear";
+    }
+}, 1500);
+
+
+// JS Doom Event Listener untuk tombol bantuan kembali ke halaman atas
+button.addEventListener("click", () => document.documentElement.scrollTop = 0);
